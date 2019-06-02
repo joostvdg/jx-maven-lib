4cf55ab71be5606c9659e1a15f01a3251c115a8a
Joost van der Griendt <joostvdg@gmail.com>
2019-06-03 00:31:51 +0200

    add lib file for demo purposes

---
+++ src/main/java/com/github/joostvdg/jx/lib/Lib.java
@@ -0,0 +1,25 @@
+package com.github.joostvdg.jx.lib;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lib 
+{
+    private final String greeting;
+
+    /**
+     * Default no-arg constructor
+     */
+    public Lib() {
+        greeting = "Hello World!";
+    }
+
+    /**
+     * This gives you a greeting!
+     * @return the greeting string
+     */
+    public String getGreeting() {
+        return greeting;
+    }
+}
