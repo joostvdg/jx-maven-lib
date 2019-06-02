78d46126a680ca2b8829dbf2354627883e0f9b44
Joost van der Griendt <joostvdg@gmail.com>
2019-06-02 17:38:03 +0200

    initial commit

---
+++ src/main/java/com/github/joostvdg/jx/App.java
@@ -0,0 +1,13 @@
+package com.github.joostvdg.jx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
